a9d1cc85e3499acbbeea548b6d93ef31c68675e2
Luca Giozza <luca.giozza@gmail.com>
2021-05-28 17:45:27 +0200

    correzione bonus

--- js/bonus.js
+++ js/bonus.js
@@ -1,5 +1,14 @@
 
 
-for(var i = 1; i <= 10; i++){
-    console.log(i * 3 );
+// for(var i = 1; i <= 10; i++){
+//     console.log(i * 3 );
+// }
+for( i = 12; i < 31; i += 3){
+    console.log(i);
 }
+
+// for(var i = 0; i < 20; i++){
+//     if(i % 2 != 0){
+//         console.log(i);
+//     }
+// }
