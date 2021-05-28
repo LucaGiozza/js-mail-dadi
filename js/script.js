a873d609b25a487a122030253054ac685bcd0064
Luca Giozza <luca.giozza@gmail.com>
2021-05-28 14:31:57 +0200

    fine giochino con risultato utente/computer

--- js/script.js
+++ js/script.js
@@ -4,16 +4,33 @@
 //   punteggio più alto.
 
 
-  var utente = Math.floor(Math.random() *6) + 1
+// generare un numero random per l'utente
+
+
+  var utente = Math.floor(Math.random() *6) + 1;
 
   console.log(utente);
 
-  var computer = Math.floor(Math.random() *6) + 1
+
+//   generare un numero random per il computer
+
+  var computer = Math.floor(Math.random() *6) + 1;
   console.log(computer);
+  var risultato ;
+
+//   stabilire il vincitore in base al punteggio più alto
 
   if(utente > computer ){
-      console.log('Hai vinto');
+      risultato = 'Hai vinto, solo fortuna..';
 
-  } else{
-      console.log('Hai perso');
+  } else if(computer > utente){
+      risultato = 'Hai perso, come sempre è stato facile';
+  }else{
+      risultato = 'Hai pareggiato, fossi in te mi accontenterei..'
   }
+
+  document.getElementById('numeri').innerHTML= risultato;
+  document.getElementById('giocatore').innerHTML= utente;
+  document.getElementById('cpu').innerHTML= computer;
+  
+  
